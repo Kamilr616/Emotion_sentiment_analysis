f1e8313912e4511a97f6a2fc574051e4161c0468
kamilr616 <kamilr616@gmail.com>
2024-04-03 15:42:11 +0200

    project base files

---
+++ source/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# main.py
+
+
+__author__ = "Kamil Rataj"
+__credits__ = []
+__version__ = "0.0.1"
+__maintainer__ = "Kamil Rataj"
+__status__ = "Development"
+
+
+def main():
+    print('Emotion sentiment analysis')
+
+
+if __name__ == '__main__':
+    main()
